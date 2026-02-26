9b59cf95b3bcca144a6c9a440d4273cf97e82846
张鑫涛 <18603592197@163.com>
2026-02-26 10:28:22 +0800

    初始化水果管理系统

---
+++ FruitManager.java
@@ -0,0 +1,11 @@
+public class FruitManager {
+    public static void main(String[] args) {
+        System.out.println("=== 辽宁分拨中心库存系统 ===");
+        displayInventory();
+    }
+
+    public static void displayInventory() {
+        // 待添加库存列表
+        System.out.println("当前库存：苹果 100 斤");
+    }
+}
